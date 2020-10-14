713a56e6dfb5c7de4fc8a806933e03d18ca4f4fa
Otaku619 <singh.amneesh1784@gmail.com>
2020-10-15 01:09:13 +0530

    added iqdb

--- cogs/fun.py
+++ cogs/fun.py
@@ -1,37 +1,36 @@
 import discord
 from discord.ext import commands
 
+
 class fun(commands.Cog):
     def __init__(self, client):
         self.client = client
 
     @commands.command()
     async def bheem(self, ctx):
-        await ctx.channel.purge(limit=1)
         await ctx.send('Bheem :revolving_hearts: ki :confounded: shakti :mag_right: dhoom :bathtub: machaye ❗️ Samne :brown_circle: koi 👨‍tik ? ?na ? ?paaye :scarf: Chhota ? ?bheem, :twisted_rightwards_arrows: chhota ⚪️ bheem :angel: Chhota ️ :taurus:bheem, ⃣ ?chhota :calendar: bheem :clock3:Bheem, :person_kneeling: bheem, :diamonds: bheem ‍🎤Chhota :hole: bheem, :person_raising_hand:chhota :roll_of_paper: bheem :thermometer_face: Dil :woman:ka :stadium:ye :frowning: rangin 👨‍khane :wave:ki :dancer: machine ‍♂️Laddo :film_frames:ka ? ?sokheen :sweat_smile: Bheem, :drop_of_blood: bheem, :mortar_board: bheem 👷‍Chhota :foggy:bheem, 👯‍chhota ‍♀️bheem ‍💼Yaaro ‍🚀ka 🧖‍ye :heavy_check_mark: yaar :calling:sabka :supervillain: madad 💂‍gaar ️ :aquarius:Haa :radio_button: haa :do_not_litter: haa :person_bowing:sab :blue_square: ko :writing_hand:baaten :boy: ye :crying_cat_face: pyaar :kissing_smiling_eyes: Bheem, 🏻 bheem, :smiley: bheem :female_sign: Chhota :mrs_claus: bheem, :lock: chhota :person_pouting:bheem 👧‍Dushman :camera_with_flash:bach 👦‍ke :drooling_face: na :grin: jaaye :broken_heart: Bheem :female_sign: shakti ⃣ 1aisi :female_sign: dikhaye :womans_flat_shoe: Bheem… ‍♀️Bheem :page_facing_up: ki ‍🔧shakti :dark_sunglasses: dhoom ⚪️ machaye ‍💻Samne :sa:koi :track_next: :track_previous:tik ? ?na ? ?pae :sweat: Chhota :pencil: bheem, :writing_hand:chhota :safety_pin: bheem :balloon: Chhota :flags: bheem, :kissing_closed_eyes: chhota ? ?bheem ‍⚕️Bheem, :person_getting_massage: bheem, 👩‍bheem :sound: Chhota 👨‍bheem, ?️ chhota :green_heart: bheem. :person_wearing_turban:')
 
     @commands.command()
     async def pubg(self, ctx):
-        await ctx.channel.purge(limit=1)
         await ctx.send('Mai :point_left:oppo:grin: ya:sweat_smile: vivo:blush: ka:star_struck: koi :exploding_head:sasta:currency_exchange: sa:briefs: phone:telephone_receiver: kharidunga:credit_card: fir:kissing_closed_eyes: usme:calling: low :sound:setting:gear: me :man:pubg:man_dancing: mobile:iphone: install :floppy_disk:krunga:wink: chowk :bike:pr:vertical_traffic_light: baithe:monkey:rickshawalon :shinto_shrine:ke :men_with_bunny_ears_partying:sath :smiling_face_with_3_hearts:squad:family_wwbb: khelunga 2-3:three: bot:space_invader: marunga :knife:wo:bat: bhi :monkey_face:aim:o: assist:diamonds: se:radioactive: aur :laughing:fir:persevere: insta:camera: ke:movie_camera: bio :notepad_spiral:me:hamster: pro :top:gamer :video_game:likhunga:pen_ballpoint: aur:confetti_ball: usi :point_down:page:page_with_curl: par :envelope_with_arrow:patt :camera_with_flash:se:aerial_tramway: headshot :bust_in_silhouette:clips :movie_camera:upload:arrow_up: krunga :guard:aise:stuck_out_tongue_closed_eyes: hi:smirk: 2:1234: din:sunny: me:frog: influencer :necktie:aur:innocent: youtuber :arrow_forward:ban:wink: jaunga:sunglasses: aur :hugging:gyan:yin_yang: chodunga:anger: dher:moneybag: saare:pound: paise:dollar: kamaunga:chart: laaj :worried:aur:sleeping: sharam:frowning2: naam:no_pedestrians: ki:racehorse: cheeze:dog2: bhul :flushed:chuka:neutral_face: hounga:sob: China:congratulations: ke:persevere: sath:smirk: dange:punch: honge:raised_hands: to:eyes: #boycotchinizeproducts:angry: aur :smiling_imp:#ckmkb :dizzy_face:likhunga:pencil2: gyan:100: chodunga:kissing_cat: aur:innocent: pubg:poop: ko :last_quarter_moon_with_face:chor:maple_leaf: kar:star2: saare :cow:chinese :accept:app:iphone: uninstall :put_litter_in_its_place:kr:pill: dunga:scissors:, phone:computer: jala :fire:dunga :tropical_drink:aur:grimacing: proud :triumph:Indian :flag_in:army:guard:ki:hushed: dekhbhakti:angry: chodunga:sunglasses:')
-    
+
     @commands.command()
     async def laundiya(self, ctx):
-        await ctx.channel.purge(limit=1)
         await ctx.send('Laundiyo:girl: ka:point_left_tone1: chod:angel: hi:hand_splayed_tone1: hota hai:ear_tone1: bekaar:thumbsdown_tone1: ek dum:speech_balloon: behen:two_women_holding_hands: ki lodi:point_up_tone1: randiya:sweat_drops: saali:dash: pyaar:two_hearts: mei fasa:palms_up_together_tone1: leti hai:heavy_check_mark: zindgiya:earth_asia: barbaad:volcano: karr deti hai:fire: fir apna:monkey: mast zindagi:dizzy: kaatti:scissors: hai:last_quarter_moon_with_face:, behen:paperclips: ke laude:banana: hum yaha:house: pagal:pig: hoe:pouting_cat: rehte:man_raising_hand_tone1: hai madarchod:peach: saale bohot:100: dukh:pensive: hota hai:broken_heart: parr jaane:open_hands_tone1: do ab:v_tone1: kya:man_shrugging_tone1: kare hai:point_right_tone1: hum:two_men_holding_hands: emotional:eyes: thode se:cry:.')
 
     @commands.command()
     async def spotify(self, ctx):
-        await ctx.channel.purge(limit=1)
         await ctx.send("Me and the band have known each other since we were kids:child::child:, we live in the same town:night_with_stars:, work at the same bar:beers:, when we go on tour it's like a little adventure :man_running::man_running::man_running:and we like to go in style:kiss:, we get out on the road :motorway::motorway:and play our music loud:loud_sound::loud_sound::loud_sound::loud_sound:, we don't need the interruptions...:no_entry_sign::no_entry_sign::no_entry_sign::no_bell::mute:")
-    
-    @commands.command(aliases=['whoasked','nooneasked', ])
+
+    @commands.command(aliases=['whoasked', 'nooneasked', ])
     async def noone(self, ctx):
-        await ctx.channel.purge(limit=1)
         await ctx.send(file=discord.File('./imgs/who.jpg'))
 
     # @commands.command()
     # async def nitro_check(self, ctx):
     #     await ctx.send(file=discord.File('./imgs/video0_6.mov'))
+
+
 def setup(client):
     client.add_cog(fun(client))
+
--- cogs/sauce.py
+++ cogs/sauce.py
@@ -5,7 +5,7 @@ import json
 import random
 import requests
 import urllib.parse
-# from BeautifulSoup import BeautifulSoup
+from bs4 import BeautifulSoup
 file_ = open('choice.json')
 choice_ = json.load(file_)
 a = 5
@@ -16,11 +16,29 @@ class sauce(commands.Cog):
         self.client = client
         self.sauce = SauceNao()
 
+    def fix_extension(self, url):
+        url = url.split('.')
+        return ('.').join(url[0:-1])+'.png?'+url[-1].split('?')[-1]
+
     @commands.command(aliases=['snao', ])
-    async def saucenao(self, ctx, *, url: str):
+    async def saucenao(self, ctx, *, url: str = ''):
         try:
-            results = self.sauce.from_url(url)
-            if (len(results) < 2):
+            url_ = ctx.message.attachments[0].url if len(
+                ctx.message.attachments) > 0 else url
+            if url_.strip() == '':
+                await ctx.send(embed=discord.Embed(title="Command: saucenao",
+                                                   colour=int(random.choice(
+                                                       choice_['COLOURS']), 16)
+                                                   ).add_field(name='aliases : ',
+                                                               value='snao',
+                                                               inline=False
+                                                               ).add_field(
+                    name='Usage :\n',
+                    value=';saucenao <image_url>'))
+                return
+            url_ = self.fix_extension(url_)
+            results = self.sauce.from_url(url_)
+            if len(results) < 2:
                 await ctx.send("No sauce found")
                 return
             best = results[0]
@@ -48,29 +66,54 @@ class sauce(commands.Cog):
         if isinstance(error, commands.BadArgument):
             await ctx.send('You need to put an URL not your dead\
                            brain you retard')
-        elif isinstance(error, commands.MissingRequiredArgument):
-            await ctx.send(embed=discord.Embed(title="Command: saucenao",
-                                               colour=int(random.choice(
-                                                   choice_['COLOURS']), 16)
-                                               ).add_field(name='aliases : ',
-                                                           value='snao',
-                                                           inline=False
-                                                           ).add_field(
-                                                               name='Usage :\n',
-                                                               value=';saucenao <image_url>'))
 
     @commands.command(aliases=[])
-    async def iqdb(self, ctx, *, url: str):
+    async def iqdb(self, ctx, *, url: str = ''):
         try:
-            searchURL = f'https://danbooru.iqdb.org/?url={url}'
-
-            params_ = {'format': 'json', }
-            response = requests.get(searchURL, params=params_)
-            response = str(response.content)
-            # response.findAll
-            print(response)
+            url_ = ctx.message.attachments[0].url if len(
+                ctx.message.attachments) > 0 else url
+            if url_.strip() == '':
+                await ctx.send(embed=discord.Embed(title="Command: iqdb",
+                                                   colour=int(random.choice(
+                                                       choice_['COLOURS']), 16)
+                                                   ).add_field(name='aliases : ',
+                                                               value='None',
+                                                               inline=False
+                                                               ).add_field(
+                    name='Usage :\n',
+                    value=';iqdb <image_url>'))
+                return
+            url_ = self.fix_extension(url_)
+            searchURL = f'https://iqdb.org/?url={url_}'
+            soup = BeautifulSoup(requests.get(searchURL).text, 'lxml')
+            sauce = soup.find('div', class_='pages')
+            nomatch = sauce.find('div', class_='nomatch') is None
+            sauce = sauce.find_all('div')[1 if nomatch else 2].find(
+                'table').find_all('tr')
+            embed_ = discord.Embed(
+                colour=int(random.choice(choice_['COLOURS']),
+                           16),
+                title='Best Match' if nomatch else 'No good match found :(',
+                url=('https:' if sauce[1].td.a.get('href')[0] != 'h' else '') +
+                sauce[1].td.a.get("href")).set_image(
+                url=('https://www.iqdb.org'
+                     if sauce[1].td.a.img.get('src')[0] != 'h' else '') +
+                sauce[1].td.a.img.get("src")).add_field(
+                name='Similarity: ', value=sauce[4].td.text.split(' ')[0],
+                inline=False).add_field(
+                name='Want more results?',
+                value=f'[Click here for all search results]({searchURL})',
+                inline=0)
+            await ctx.send(embed=embed_)
         except Exception as e:
             print(e)
+            await ctx.send("Invalid URL or couldn't find")
+
+    @ iqdb.error
+    async def clear_iqdb_error(self, ctx, error):
+        if isinstance(error, commands.BadArgument):
+            await ctx.send('You need to put an URL not your dead\
+                           brain you retard')
 
 
 def setup(client):
